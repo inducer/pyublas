6e519046d207dc42d525e832beb3208ef9d7535f
Andreas Kloeckner <inform@tiker.net>
2008-03-23 16:00:09 -0400

    Rid build system of all unused dependencies.

--- test/do_test.py
+++ test/do_test.py
@@ -5,3 +5,8 @@ a = numpy.array([[1,2],[3,4]])
 print a.dtype
 print a
 print dblmat(a)
+
+from pyublas import sparse
+u =  sparse({2:{3:17}}, shape=(6,6))
+v =  sparse({5:{2:17}}, shape=(6,6))
+print u+v
