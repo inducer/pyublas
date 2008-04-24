a1e4c1ab92f5874867260d9bd2c97499ce1479a9
Andreas Kloeckner <inform@tiker.net>
2008-04-23 21:17:03 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def main():
 
     setup(
             name="PyUblas",
-            version="0.92.2",
+            version="0.92.3",
             description="Seamless Numpy-UBlas interoperability",
             long_description="""
             PyUblas provides a seamless glue layer between
