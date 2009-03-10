53eb5537774bdb3949b98f99ff0b67fdfadebfdd
Andreas Kloeckner <inform@tiker.net>
2009-03-10 11:46:51 -0400

    Update aksetup.

--- setup.py
+++ setup.py
@@ -6,13 +6,11 @@
 
 def get_config_schema():
     from aksetup_helper import ConfigSchema, Option, \
-            IncludeDir, LibraryDir, Libraries, \
-            Switch, StringListOption
+            IncludeDir, LibraryDir, Libraries, BoostLibraries, \
+            Switch, StringListOption, make_boost_base_options
 
-    return ConfigSchema([
-        IncludeDir("BOOST", []),
-        LibraryDir("BOOST", []),
-        Libraries("BOOST_PYTHON", ["boost_python-gcc42-mt"]),
+    return ConfigSchema(make_boost_base_options() + [
+        BoostLibraries("python"),
 
         Switch("WITH_SPARSE_WRAPPERS", False, "Whether to build sparse wrappers"),
         Switch("USE_ITERATORS", False, "Whether to use iterators (faster, requires new Boost)"),
